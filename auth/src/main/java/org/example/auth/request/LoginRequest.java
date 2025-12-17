432a6f57982338e6bdb421d8fc124352d7409759
MR Ferry <ferferfx@gmail.com>
2025-12-18 05:52:59 +0700

    first commit

---
+++ auth/src/main/java/org/example/auth/request/LoginRequest.java
@@ -0,0 +1,9 @@
+package org.example.auth.request;
+
+/************************
+ * Made by [MR Ferry™]  *
+ * on Desember 2025     *
+ ************************/
+
+public record LoginRequest(String username, String password){
+}
